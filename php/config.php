58d3986f22ca332ca508bad7c974553ca436a6ab
JuanCon1 <juancongar@hotmail.com>
2023-10-09 17:16:51 -0600

    pruba de cambio de nombre

--- php/config.php
+++ php/config.php
@@ -3,7 +3,7 @@ $hostname = "localhost";
 $username = "root";
 $password = "";
 $dbname = "chat-pasalista";
-
+////////////////////////////////////////////////////////////////////////
 $conn = mysqli_connect($hostname, $username, $password, $dbname);
 if (!$conn) {
   echo "Database connection error" . mysqli_connect_error();
